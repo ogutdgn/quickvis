76869d59a7ceb8221b01a64624ad537e5b23dbd9
ogutdgn <ogutdgn@gmail.com>
2025-12-08 01:40:55 -0600

    restructured files and worked on CSP (Content Security Policy)

--- extension/background.js
+++ extension/background.js
@@ -1,4 +1,10 @@
-importScripts('config.js');
+const CONFIG = {
+  CLIENT_ID: 'Ov23liEPqc4XAqT5VB1Q',
+  PROXY_SERVER_URL: 'https://quickvis-oauth-proxy.vercel.app',
+  SCOPES: 'repo user',
+  AUTH_URL: 'https://github.com/login/oauth/authorize',
+  API_BASE: 'https://api.github.com'
+};
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === 'login') {
