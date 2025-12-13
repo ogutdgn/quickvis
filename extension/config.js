1161257ed02de94a71e90772823fcc024903fa37
ogutdgn <ogutdgn@gmail.com>
2025-12-13 05:28:25 -0600

    Proxy Server URL changed to quickvis.vercel.app

--- extension/config.js
+++ extension/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
   CLIENT_ID: 'Ov23lieBdpAuzLEPM61t',
-  PROXY_SERVER_URL: 'https://quickvis-oauth-proxy.vercel.app',
+  PROXY_SERVER_URL: 'https://quickvis.vercel.app/',
   SCOPES: 'repo user',
   AUTH_URL: 'https://github.com/login/oauth/authorize',
   API_BASE: 'https://api.github.com'
