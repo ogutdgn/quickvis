fec804fffd666584154c188c42e0bccd2f72cab7
ogutdgn <ogutdgn@gmail.com>
2025-12-23 01:14:50 -0600

    revoke pop-up UI improved

--- extension/content.js
+++ extension/content.js
@@ -197,8 +197,8 @@
         <div class="quickvis-modal-content">
           <div class="quickvis-modal-left quickvis-modal-left-warning">
             <div class="quickvis-modal-icon">
-              <svg width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                <path d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"/>
+              <svg width="32" height="32" viewBox="0 0 24 24" fill="currentColor">
+                <path fill-rule="evenodd" d="M9.401 3.003c1.155-2 4.043-2 5.197 0l7.355 12.748c1.154 2-.29 4.5-2.599 4.5H4.645c-2.309 0-3.752-2.5-2.598-4.5L9.4 3.003zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"/>
               </svg>
             </div>
             <h2>Revoke Access</h2>
