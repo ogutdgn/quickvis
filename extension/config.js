370b80ea762917985da9ee25aef181efa8bdc5ee
ogutdgn <ogutdgn@gmail.com>
2025-12-23 00:09:19 -0600

    proxy server url changed

--- extension/config.js
+++ extension/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
   CLIENT_ID: 'Ov23lieBdpAuzLEPM61t',
-  PROXY_SERVER_URL: 'https://quickvis-oauth-proxy-h4gb343t1-dogans-projects-2fe10410.vercel.app',
+  PROXY_SERVER_URL: 'https://quickvis-oauth-proxy-dqjzc9pdh-dogans-projects-2fe10410.vercel.app',
   SCOPES: 'repo user',
   AUTH_URL: 'https://github.com/login/oauth/authorize',
   API_BASE: 'https://api.github.com'
