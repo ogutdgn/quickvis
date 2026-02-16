d792a15e3cb786cdf9a2294e6bcaa8c41624bf04
ogutdgn <ogutdgn@gmail.com>
2026-02-15 23:09:46 -0600

    chore: setup local development environment
    - Configure config.js to use localhost:3000
    - Update manifest.json host_permissions for localhost
    - Use dev OAuth client ID

--- extension/config.js
+++ extension/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
-  CLIENT_ID: 'Ov23lieBdpAuzLEPM61t',
-  PROXY_SERVER_URL: 'https://quickvis-oauth-proxy.vercel.app',
+  CLIENT_ID: 'Ov23liljIVCTB62SKfe2',
+  PROXY_SERVER_URL: 'http://localhost:3000',
   SCOPES: 'repo user',
   AUTH_URL: 'https://github.com/login/oauth/authorize',
   API_BASE: 'https://api.github.com'
